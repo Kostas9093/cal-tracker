8f3047daad3afa37a75ef72e57d03049cfa59cfa
Kostas9093 <159778628+Kostas9093@users.noreply.github.com>
2025-07-17 18:54:29 +0100

    e

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', // important for GitHub Pages and Median
+  base: './',
   plugins: [react()],
 });
